0c96f7d85e85edb89c9cafba45218f31132a7e16
Damjan Dikic <damjand48@gmail.com>
2024-10-25 15:24:58 +0200

    Initial commit

---
+++ src/main/java/com/ISAproject/OnlyBuns_app/OnlyBunsAppApplication.java
@@ -0,0 +1,13 @@
+package com.ISAproject.OnlyBuns_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlyBunsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlyBunsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ISAproject/OnlyBuns_app/OnlyBunsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ISAproject.OnlyBuns_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlyBunsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
